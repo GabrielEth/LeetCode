a09a3c573a0378ccb47ac7f741f4fa5985c995f8
Gabriel Garza Garza <97363338+GabrielEth@users.noreply.github.com>
2022-10-11 21:30:52 -0500

    Time: 59 ms (97.07%), Space: 42 MB (87.31%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+let invertTree = function(root) {
+    if(!root || !root.left && !root.right){
+        return root;
+    }
+    
+    const oldLeft = root.left;
+    root.left = root.right;
+    root.right = oldLeft;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+};
